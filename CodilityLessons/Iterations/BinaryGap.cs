7963fda3efa4d69a749117fef3c2666987fc7118
chitangchin <chitangb.chin@gmail.com>
2024-08-19 12:32:48 -0400

    Codility: BinaryGap

---
+++ CodilityLessons/Iterations/BinaryGap.cs
@@ -0,0 +1,40 @@
+﻿namespace CodilityLessons.Iterations.BinaryGap
+{
+    public class SolutionClass
+    {
+        public static int BinaryGap(int N)
+        {
+            //Convert decimal N into binary
+            string binaryN = DecimalToBinary(N);
+
+            int maxCounter = 0;
+            int zeroCounter = 0;
+
+            for (int i = 0; i < binaryN.Length; i++)
+            {
+                if (binaryN[i] == '1')
+                {
+                    if (zeroCounter > maxCounter)
+                    {
+                        maxCounter = zeroCounter;
+                    }
+                    zeroCounter = 0;
+                }
+                else
+                {
+                    zeroCounter++;
+                }
+            }
+            return maxCounter;
+        }
+        public static string DecimalToBinary(int N)
+        {
+            if (N < 2) return N.ToString();
+
+            int divisor = N / 2;
+            int remainder = N % 2;
+
+            return DecimalToBinary(divisor) + remainder;
+        }
+    }
+}
---
+++ CodilityLessonsTest/Iterations/BinaryGapTest.cs
@@ -0,0 +1,19 @@
+﻿using CodilityLessons.Iterations.BinaryGap;
+
+namespace CodilityLessonsTest.Iterations
+{
+    [TestFixture]
+    public class BinaryGapTests
+    {
+        [Test]
+        public void RunTests()
+        {
+            Assert.That(SolutionClass.BinaryGap(15), Is.EqualTo(0));
+        }
+        [Test]
+        public void LargeTests()
+        {
+            Assert.That(SolutionClass.BinaryGap(1041), Is.EqualTo(5));
+        }
+    }
+}
