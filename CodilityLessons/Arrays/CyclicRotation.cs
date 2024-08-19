eada300522822e028e670ae2ca244f706b8684c5
chitangchin <chitangb.chin@gmail.com>
2024-08-19 14:47:37 -0400

    Lesson 2: Array - CyclicRotation

---
+++ CodilityLessons/Arrays/CyclicRotation.cs
@@ -0,0 +1,29 @@
+﻿namespace CodilityLessons.Arrays.CyclicRotation
+{
+    public class SolutionClass
+    {
+        public static int[] solution(int[] A, int K)
+        {
+            // Implement your solution here
+            if (A == null)
+            {
+                return A;
+            }
+            //Observation: each index is +1 except for the last index. last index should be 0
+            //Shift k times
+            while (K > 0)
+            {
+                int lastElement = A[A.Length - 1];
+
+                for (int i = A.Length - 1; i > 0; i--)
+                {
+                    A[i] = A[i - 1];
+
+                }
+                A[0] = lastElement;
+                K--;
+            }
+            return A;
+        }
+    }
+}
---
+++ CodilityLessonsTest/Arrays/CyclicRotationTest.cs
@@ -0,0 +1,18 @@
+﻿using CodilityLessons.Arrays.CyclicRotation;
+
+namespace CodilityLessonsTest.Arrays
+{
+    public class CyclicRotationTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.solution(new int[] { 3, 8, 9, 7, 6 }, 3), Is.EqualTo(new int[] { 9,7,6,3,8 }));
+        }
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.That(SolutionClass.solution(new int[] { }, 3), Is.EqualTo(new int[] { }));
+        }
+    }
+}
