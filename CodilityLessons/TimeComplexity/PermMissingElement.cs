b285a61fdce7c06c78c80e6fa2844b3dd93769ae
chitangchin <chitangb.chin@gmail.com>
2024-08-21 15:26:32 -0400

    Completed Time Complexity: PermMissingElement

---
+++ CodilityLessons/TimeComplexity/PermMissingElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityLessons.TimeComplexity.PermMissingElement
+{
+    public class SolutionClass
+    {
+        public static int PermMissingEl(int[] A)
+        {
+            // Implement your solution here
+            int permutation = 0;
+            //Sum all the elements
+            for (int i = 0; i < A.Length; i++)
+            {
+                //add to the permutation
+                permutation += i + 1;
+                permutation -= A[i];
+            }
+            permutation += A.Length + 1;
+            return permutation;
+        }
+    }
+}
---
+++ CodilityLessonsTest/TimeComplexity/PermMissingElementTest.cs
@@ -0,0 +1,18 @@
+﻿using CodilityLessons.TimeComplexity.PermMissingElement;
+
+namespace CodilityLessonsTest.TimeComplexity
+{
+    public class PermMissingElementTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(SolutionClass.PermMissingEl([1, 3, 2, 5]), Is.EqualTo(4));
+        }
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.That(SolutionClass.PermMissingEl([]), Is.EqualTo(1));
+        }
+    }
+}
