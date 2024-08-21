2df5dc81c707fdf2049b1f476936101571654ea5
chitangchin <chitangb.chin@gmail.com>
2024-08-21 10:55:16 -0400

    Added Tests for frogjmp

--- CodilityLessonsTest/TimeComplexity/FrogjmpTest.cs
+++ CodilityLessonsTest/TimeComplexity/FrogjmpTest.cs
@@ -9,5 +9,20 @@ namespace CodilityLessonsTest.TimeComplexity
         {
             Assert.That(SolutionClass.Solution(1,2,2), Is.EqualTo(1));
         }
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.Solution(1101, 120302031, 59), Is.EqualTo(2038999));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.That(SolutionClass.Solution(100, 250, 100), Is.EqualTo(2));
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.That(SolutionClass.Solution(99, 210, 62), Is.EqualTo(2));
+        }
     }
 }
