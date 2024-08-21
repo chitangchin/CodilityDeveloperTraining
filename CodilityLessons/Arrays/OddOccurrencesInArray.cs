b65cd3c49df81e40b1b395cbcb2e9f575c41c85b
brandon chin <chitangb.chin@gmail.com>
2024-08-20 22:42:00 -0400

    Arrays: OddOccurrencesInArray

---
+++ CodilityLessons/Arrays/OddOccurrencesInArray.cs
@@ -0,0 +1,30 @@
+﻿namespace CodilityLessons.Arrays.OddOccurrencesInArray
+{
+    public class SolutionClass
+    {
+        public static int OddOccurrences(int[] A)
+        {
+            //Create a dictionary
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            foreach (int num in A)
+            {
+                if (map.ContainsKey(num))
+                {
+                    map.Remove(num);
+                }
+                else
+                {
+                    map.Add(num, 1);
+                }
+            }
+
+            int result = 0;
+            foreach (KeyValuePair<int, int> num in map)
+            {
+                result += num.Key;
+            }
+            return result;
+        }
+    }
+}
---
+++ CodilityLessonsTest/Arrays/OddOccurrencesInArrayTest.cs
@@ -0,0 +1,13 @@
+﻿using CodilityLessons.Arrays.OddOccurrencesInArray;
+
+namespace CodilityLessonsTest.Arrays
+{
+    public class OddOccurrencesInArrayTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.OddOccurrences(new int[] {3,3,9,9,9,9,7}), Is.EqualTo(7));
+        }
+    }
+}
