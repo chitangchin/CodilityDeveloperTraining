2911a0221212d3178720d203d86b605c6f3115f6
chitangchin <chitangb.chin@gmail.com>
2024-08-21 10:53:17 -0400

    Time Complexity: Frog Jump

---
+++ CodilityLessons/TimeComplexity/Frogjmp.cs
@@ -0,0 +1,13 @@
+﻿namespace CodilityLessons.TimeComplexity.Frogjmp
+{
+    public class SolutionClass
+    {
+        public static int Solution(int X, int Y, int D)
+        {
+            // Implement your solution here
+            decimal distance = Y - X;
+            decimal result = Math.Ceiling(distance / D);
+            return (int)result;
+        }
+    }
+}
---
+++ CodilityLessonsTest/TimeComplexity/FrogjmpTest.cs
@@ -0,0 +1,13 @@
+﻿using CodilityLessons.TimeComplexity.Frogjmp;
+
+namespace CodilityLessonsTest.TimeComplexity
+{
+    public class FrogjmpTest
+    {
+        [Test]
+        public void Test() 
+        {
+            Assert.That(SolutionClass.Solution(1,2,2), Is.EqualTo(1));
+        }
+    }
+}
