1b50cffb75f486c713072028e6bc4760b3c4b096
chitangchin <chitangb.chin@gmail.com>
2024-08-26 10:25:50 -0400

    Counting Elements: PermCheck

---
+++ CodilityLessons/CountingElements/PermCheck.cs
@@ -0,0 +1,20 @@
+﻿namespace CodilityLessons.CountingElements.PermCheck
+{
+    public class PermCheck
+    {
+        public static int Solution(int[] A)
+        {
+            //None empty array with N Integers given
+            //Goal: check if the array is a permutation or not
+
+            // Implement your solution here
+            Array.Sort(A);
+            if (A[0] != 1) return 0;
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (A[i] - 1 != A[i - 1]) return 0;
+            }
+            return 1;
+        }
+    }
+}
---
+++ CodilityLessonsTest/CountingElements/PermCheckTest.cs
@@ -0,0 +1,23 @@
+﻿using CodilityLessons.CountingElements.PermCheck;
+
+namespace CodilityLessonsTest.CountingElements
+{
+    public class PermCheckTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(PermCheck.Solution([1,2,3,4,5,7]), Is.EqualTo(0));
+            Assert.That(PermCheck.Solution([1, 2, 3, 4, 5, 6]), Is.EqualTo(1));
+            Assert.That(PermCheck.Solution([1, 2, 3, 4, 5, 6, 7, 8, 9]), Is.EqualTo(1));
+            Assert.That(PermCheck.Solution([1, 3, 4, 5, 7]), Is.EqualTo(0));
+        }
+        public void ClarifyingTest()
+        {
+            Assert.That(PermCheck.Solution([8]), Is.EqualTo(0));
+            Assert.That(PermCheck.Solution([1,5]), Is.EqualTo(0));
+            Assert.That(PermCheck.Solution([7,8]), Is.EqualTo(0));
+            Assert.That(PermCheck.Solution([1]), Is.EqualTo(1));
+        }
+    }
+}
