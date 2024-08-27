a53d461a6ba41d78705c214bcdc3b383bddda255
chitangchin <chitangb.chin@gmail.com>
2024-08-27 16:36:00 -0400

    Counting Elements: Missing Integer

---
+++ CodilityLessons/CountingElements/MissingInteger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityLessons.CountingElements.MissingInteger
+{
+    public class SolutionClass
+    {
+        public static int MissingInteger(int[] A)
+        {
+            int numCheck = 1;
+            bool found = false;
+            Array.Sort(A);
+
+            while (found == false)
+            {
+                if (Array.BinarySearch(A, numCheck) < 0)
+                {
+                    found = true;
+                    break;
+                }
+                numCheck += 1;
+            }
+            return numCheck;
+        }
+    }
+}
---
+++ CodilityLessonsTest/CountingElements/MissingIntegerTest.cs
@@ -0,0 +1,17 @@
+﻿using CodilityLessons.CountingElements.MissingInteger;
+
+namespace CodilityLessonsTest.CountingElements
+{
+    public class MissingIntegerTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(SolutionClass.MissingInteger([1, 3, 6, 4, 1, 2]), Is.EqualTo(5));
+            Assert.That(SolutionClass.MissingInteger([-1, 3, 6, 4, 1, 2]), Is.EqualTo(5));
+            Assert.That(SolutionClass.MissingInteger([-14,-12,1,43]), Is.EqualTo(2));
+            Assert.That(SolutionClass.MissingInteger([100,000]), Is.EqualTo(1));
+            Assert.That(SolutionClass.MissingInteger([5,4,3,2,1]), Is.EqualTo(6));
+        }
+    }
+}
