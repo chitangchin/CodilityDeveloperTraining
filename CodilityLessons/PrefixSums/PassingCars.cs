7231c433eb4349aaf34076354c45e8c7a78e5f3f
chitangchin <chitangb.chin@gmail.com>
2024-08-28 14:08:23 -0400

    Prefix Sum: Counting Cars

---
+++ CodilityLessons/PrefixSums/PassingCars.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityLessons.PrefixSums.PassingCars
+{
+    public class SolutionClass
+    {
+        public static int PassingCars(int[] A)
+        {
+            /*
+             Given: non empty array A
+            N integers given
+
+            0s or 1s
+
+            0: car traveling east
+            1: car traveling west
+            0 1 2 3 4  
+            >   > 
+              <   < <
+             */
+            //how many 1's are there in an index greater and equal to 0? sum th passes
+            int sumPasses = 0;
+            int eastCars = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 0) eastCars += 1;
+                if (A[i] == 1) sumPasses += eastCars;
+                if (sumPasses > 1000000000) return -1;
+            }
+            return sumPasses;
+        }
+    }
+}
---
+++ CodilityLessonsTest/PrefixSums/PassingCarsTest.cs
@@ -0,0 +1,13 @@
+﻿using CodilityLessons.PrefixSums.PassingCars;
+
+namespace CodilityLessonsTest.PrefixSums
+{
+    public class PassingCarsTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(SolutionClass.PassingCars([0,1,0,1,1]), Is.EqualTo(5));
+        }
+    }
+}
