cc0a1574bc9743c687ca6be41ecf6b07e6e185af
chitangchin <chitangb.chin@gmail.com>
2024-08-29 09:24:11 -0400

    PrefixSums: CountingDiv

---
+++ CodilityLessons/PrefixSums/CountingDiv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityLessons.PrefixSums.CountingDiv
+{
+    public class SolutionClass
+    {
+        public static int Solution(int A, int B, int K)
+        {
+            // Implement your solution here
+            int bDivisibleByK = B / K;
+            int aDivisibleByK = A / K;
+            int inclusive = (A % K == 0 ? 1 : 0);
+            return bDivisibleByK - aDivisibleByK + inclusive;
+        }
+    }
+}
---
+++ CodilityLessonsTest/PrefixSums/CountingDivTest.cs
@@ -0,0 +1,12 @@
+﻿using CodilityLessons.PrefixSums.CountingDiv;
+namespace CodilityLessonsTest.PrefixSums
+{
+    public class CountingDivTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(SolutionClass.Solution(6, 11, 2), Is.EqualTo(3));
+        }
+    }
+}
